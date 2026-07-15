ffc96cac5e1611b47a112da854b5759fb39c4241
mrAibo <93589172+mrAibo@users.noreply.github.com>
2026-07-15 15:02:18 +0200

    Align planner license header

--- plugins/filter/serviceflow_plan.py
+++ plugins/filter/serviceflow_plan.py
@@ -1,5 +1,5 @@
-# Copyright (C) 2026 Aleksej Voronin
-# GNU General Public License v3.0 or later
+# Copyright: (c) 2026 Aleksej Voronin
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from collections.abc import Mapping, Sequence
 
